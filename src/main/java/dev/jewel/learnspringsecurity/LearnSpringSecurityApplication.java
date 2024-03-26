37fd3e9d5f5f6f074f8de05cd99f46f632aae1c6
Jewel-cse <md.jewel.cse@gmail.com>
2024-03-26 20:51:47 +0600

    setup project and project structure

---
+++ src/main/java/dev/jewel/learnspringsecurity/LearnSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package dev.jewel.learnspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jewel/learnspringsecurity/LearnSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jewel.learnspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
